a95f7bd286b2ab3defb84aa03df18545e944f379
Md. Sohel Al Masud <bappymasud9@gmail.com>
2025-12-12 02:58:33 +0600

    Add DriverFactory class for Chrome WebDriver setup

---
+++ automation/utilities/driver_factory.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+class DriverFactory:
+
+    @staticmethod
+    def get_driver():
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
+        return driver
